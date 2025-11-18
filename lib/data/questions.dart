718400c661f61b3744a581dfb09a8d6b11dd81d7
Jamie <jamie.butterworth@thisismilk.co.uk>
2025-11-18 13:25:54 +0000

    init

---
+++ lib/data/questions.dart
@@ -0,0 +1,10 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('What are the main building blocks of Flutter UIs?', [
+    'Widgets',
+    'Components',
+    'Blocks',
+    'Functions',
+  ]),
+];
